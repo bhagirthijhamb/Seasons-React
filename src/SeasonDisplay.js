9259646679a47a57de9e78d2f860f324e2b3d8f5
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-15 14:31:35 -0500

    added seasonConfig object for handling multiple ternary expressions

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+// configuration object
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake outline'
+  }
+}
+
+function getSeason(lat, month) {
+  if(month > 2 && month < 9){
+    return lat > 0 ? 'summmer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  // console.log(props);
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+  // const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+  // const iconName = season === 'winter' ? 'snowflake outline' : 'sun';
+
+  const { text, iconName } = seasonConfig[season];
   return (
-    <div>Season Display</div>
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{ text }</h1>
+      <i className={`${iconName} icon`} />
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
   // First function to be called with props object
-  constructor(props) {
-    // we re overwriting the constructor function so we
-    // refer to parents constructor function to get its functionality
-    super(props); 
-    this.state = { lat: null, errMessage: '' }
+  // constructor(props) {
+  //   // we re overwriting the constructor function so we
+  //   // refer to parents constructor function to get its functionality
+  //   super(props); 
+  //   this.state = { lat: null, errMessage: '' }
+  // }
 
+  // Bable wil build up the constructor for us like above
+  state = { lat: null, errMessage: ''}
+
+  componentDidMount(){
+    console.log('My component was rendered to the screen')
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log(position);
@@ -21,13 +28,20 @@ class App extends React.Component {
     );
   }
 
+  componentDidUpdate(){
+    console.log('My component was just updated - it rerendered');
+  }
+
   // React says we have to define render!!
   render (){
       if(this.state.errMessage && !this.state.lat){
         return <div>Error: {this.state.errMessage}</div>
       }
       if(!this.state.errMessage && this.state.lat){
-        return <div>Latitude: {this.state.lat}</div>
+        // return <div>Latitude: {this.state.lat}</div>
+        
+        // passing state as a prop to the child component
+        return <SeasonDisplay lat={this.state.lat}/>
       }
       return <div>Loading...</div>
   }
