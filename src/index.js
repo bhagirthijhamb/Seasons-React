a0a928bd2a8a177d37c785d3664c2f9e40564156
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-25 10:44:33 -0500

    made some changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// App Challenges
+// - Need to get the users physical location
+// Need to determine the current month
+// Need to chnage text and stylling based on location + month
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
@@ -6,9 +10,13 @@ import Clock from './Clock';
 
 class App extends React.Component {
   // First function to be called with props object
+  // Feature of JavaScript
+  // Not required by React
   // constructor(props) {
-  //   // we re overwriting the constructor function so we
-  //   // refer to parents constructor function to get its functionality
+      // App component is borrowing functionality fomr React.Component base calss by extending it
+      // Base class has a constructor function of its own that goes through some amount of setup to set React component for us
+  //   // When we define a constructor function, we are essentially overwriting the constructor function inside React.Component class but we still want that all the set up code inside React.Components custructor function gets called. So we call super with props
+  //   // super is a reference to parents constructor function to get its functionality
   //   super(props); 
   //   this.state = { lat: null, errMessage: '' }
   // }
@@ -62,3 +70,8 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// Geolocatio API is built into most modern browsers
+// geolocations apis getCurrentPosition() mehod makes an educated guess about where the user is based on the positioning hardware such as IP address, different wi-fi networks that are there.
+// getCurrentPosition() method takes two callbacks - success callback, failure callback
+
